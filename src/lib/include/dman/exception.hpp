8e6101252d14c43e37f6499036870925f4cb7a6b
player_legend <player_legend>
2026-05-24 22:53:28 -0500

    Rename exception namespace to common

--- src/lib/include/dman/exception.hpp
+++ src/lib/include/dman/exception.hpp
@@ -2,11 +2,11 @@
 
 #include <stdexcept>
 
-namespace exception
+namespace common
 {
 class exception : public std::runtime_error
 {
   public:
     using std::runtime_error::runtime_error;
 };
-} // namespace exception
+} // namespace common
